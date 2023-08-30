3d9b9988417761e2ca0040817445f5dd481e04c7
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-08-29 22:38:10 -0300

    refac: applying cleaning code concepts

--- js/main.js
+++ js/main.js
@@ -4,49 +4,66 @@ const lastName = document.querySelector("#last-name");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const submitForm = document.querySelector("#form-button");
-const firstNameImageError = document.querySelector("#first-name-message-error");
-const firstNameMessageError = document.querySelector("#first-name-image-error");
-const lastNameImageError = document.querySelector("#last-name-message-error");
-const lastNameMessageError = document.querySelector("#last-name-image-error");
+const firstNameImageError = document.querySelector("#first-name-image-error");
+// prettier-ignore
+const firstNameMessageError = document.querySelector("#first-name-message-error");
+const lastNameImageError = document.querySelector("#last-name-image-error");
+const lastNameMessageError = document.querySelector("#last-name-message-error");
 const emailImageError = document.querySelector("#email-image-error");
 const emailMessageError = document.querySelector("#email-message-error");
 const passwordImageError = document.querySelector("#password-image-error");
 const passwordMessageError = document.querySelector("#password-message-error");
+const error = document.querySelectorAll("small");
+const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   validateForm();
 });
 
+function validInput(message, image) {
+  message.classList.add("hide");
+  image.classList.add("hide");
+}
+
+function invalidInput(input, image, message) {
+  input.classList.remove("hide");
+  image.classList.remove("hide");
+  input.innerText = message;
+}
+
 function validateForm() {
-  const nameValue = firstName.value.trim();
-  const lastNameValue = lastName.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
-
-  function invalidInput(message, image) {
-    message.classList.remove("hide");
-    image.classList.remove("hide");
-  }
-
-  function validInput(message, image) {
-    message.classList.add("hide");
-    image.classList.add("hide");
-  }
-
-  nameValue === ""
-    ? invalidInput(firstNameMessageError, firstNameImageError)
+  firstName.value.trim() === ""
+    ? invalidInput(firstNameMessageError, firstNameImageError, "First Name cannot be empty")
     : validInput(firstNameMessageError, firstNameImageError);
 
-  lastNameValue === ""
-    ? invalidInput(lastNameMessageError, lastNameImageError)
+  lastName.value.trim() === ""
+    ? invalidInput(lastNameMessageError, lastNameImageError, "Last Name cannot be empty")
     : validInput(lastNameMessageError, lastNameImageError);
 
-  emailValue === ""
-    ? invalidInput(emailMessageError, emailImageError)
+  email.value.trim() === ""
+    ? invalidInput(emailMessageError, emailImageError, "E-mail cannot be empty")
     : validInput(emailMessageError, emailImageError);
 
-  passwordValue === ""
-    ? invalidInput(passwordMessageError, passwordImageError)
+  password.value.trim() === ""
+    ? invalidInput(passwordMessageError, passwordImageError, "Password cannot be empty")
     : validInput(passwordMessageError, passwordImageError);
+
+  if (firstName.value.length > 1 && firstName.value.length < 3) {
+    invalidInput(firstNameMessageError, firstNameImageError, "First Name must have at least 3 characters");
+  }
+
+  if (lastName.value.length > 1 && lastName.value.length < 3) {
+    invalidInput(lastNameMessageError, lastNameImageError, "Last Name must have at least 3 characters");
+  }
+
+  if (email.value.length > 1 && !email.value.match(pattern)) {
+    document.getElementById("email").placeholder = "email@example.com";
+    invalidInput(emailMessageError, emailImageError, "Looks like this is not an e-mail");
+    email.value = "";
+  }
+
+  if (password.value.length > 1 && password.value.length < 5) {
+    invalidInput(passwordMessageError, passwordImageError, "Password must have at least 4 characters");
+  }
 }
